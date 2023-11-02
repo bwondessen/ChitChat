f66b751ad26b8e8e6224b90d74d3127b7f403782
bwondessen <bwondessen23@gmail.com>
2023-11-02 18:13:19 -0500

    Initial Commit

---
+++ ChitChat/ChitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChitChatApp.swift
+//  ChitChat
+//
+//  Created by Bruke Wondessen on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ChitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
