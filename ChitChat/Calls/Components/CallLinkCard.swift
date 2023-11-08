f36c4a525df3b0c0840090928ca1a66143ad958a
bwondessen <bwondessen23@gmail.com>
2023-11-07 20:18:55 -0600

    Add feature for sharing call links and add navigation buttons

---
+++ ChitChat/Calls/Components/CallLinkCard.swift
@@ -0,0 +1,18 @@
+//
+//  CallLinkCard.swift
+//  ChitChat
+//
+//  Created by Bruke Wondessen on 11/7/23.
+//
+
+import SwiftUI
+
+struct CallLinkCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CallLinkCard()
+}
